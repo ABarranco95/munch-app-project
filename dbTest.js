a1d504f7a81e9f1fe1dc462340616d39d896f5ee
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-12-01 21:08:03 -0800

    [dbTest.js]Got api to work

--- dbTest.js
+++ dbTest.js
@@ -24,3 +24,38 @@ const API_KEY = process.env.API_KEY;
 //     console.log(profile);
 // }
 // test();
+
+
+
+
+const url = `https://api.documenu.com/v2/restaurants/zip_code/93638?key=${API_KEY}`;
+axios.get(url)
+.then(response => {
+    if (response.status === 200) {
+        // console.log(response.data);
+        let len = response.data.length;
+        for (let i = 0; i < len; i++) {
+            let searchResultObject = response.data[i];
+            const searchRestObject = {
+                restaurantName: searchResultObject.restaurant_name,
+                address: searchResultObject.address,
+                phone: searchResultObject.restaurant_phone
+
+            };
+            console.log(searchRestObject);
+            // db.favorite.findOrCreate({
+            //     where: { foodId: searchRestObject.foodId },
+            //     defaults: {
+            //         restaurantName: searchRestObject.restaurant_name,
+            //         address: searchRestObject.address.formatted,
+            //         phone: searchRestObject.phone,
+            //     }
+            // }).then(([favorite, created]) => {
+            //     console.log(created);
+            // })
+        }
+    }
+})
+.catch(err => {
+    console.log(err);
+});
